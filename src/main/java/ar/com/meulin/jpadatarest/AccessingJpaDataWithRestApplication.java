84872e987fc0267149cbcba0566bd0dc2f2abe71
Marcelo <marcelo.lafaille@algun.mail.com>
2020-08-14 14:01:14 -0300

    Initial commit

---
+++ src/main/java/ar/com/meulin/jpadatarest/AccessingJpaDataWithRestApplication.java
@@ -0,0 +1,13 @@
+package ar.com.meulin.jpadatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingJpaDataWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingJpaDataWithRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/meulin/jpadatarest/AccessingJpaDataWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.meulin.jpadatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingJpaDataWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
